2200247edbc348f46ab0ad953316c15ef876445b
mcozart303 <mikojak@gmail.com>
2026-02-02 13:50:43 -0500

    hp tweaks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,8 +14,7 @@
             <div id="pricing" class="grid grid-cols-1 lg:grid-cols-3 gap-4 px-4 lg:px-0 mx-auto max-w-7xl text-center">
                 <div class="relative bg-gray-100 dark:bg-gray-800 pt-8 pb-24 lg:pb-8 px-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
                     <h2 class="text-4xl font-bold text-gray-800 dark:text-gray-200 mb-2">Basic</h2>
-                    <h3 class="mb-4 font-bold text-3xl text-black dark:text-lime-300 mb-4"><span class="line-through text-gray-500 dark:text-gray-500">$149</span> $99/month</h3>
-                    <div class="inline-block mt-0 mb-4 px-4 py-2 bg-gray-300 dark:bg-gray-900 text-black dark:text-white rounded-md text-xs">Sale Ends 02/31</div>
+                    <h3 class="mb-4 font-bold text-3xl text-black dark:text-lime-300 mb-4">$99/month</h3>
                     <div class="flex items-center justify-center gap-3 mb-6 bg-white dark:bg-gray-700 dark:border-gray-400 bg-opacity-70 dark:bg-opacity-30 rounded-lg p-2 text-gray-700 dark:text-gray-300">
                         <svg class="w-5 h-5 text-green-500 dark:text-green-400 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21.75 6.75a4.5 4.5 0 01-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 11-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 016.336-4.486l-3.776 3.776c.03.242.066.477.098.717l3.464 3.464c.024-.137.048-.274.072-.412l2.341-2.341z"></path>
@@ -72,8 +71,7 @@
                 </div>
                 <div class="relative bg-gray-100 dark:bg-gray-800 pt-8 pb-24 lg:pb-8 px-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
                     <h2 class="text-4xl font-bold text-gray-800 dark:text-gray-200 mb-2">Pro</h2>
-                    <h3 class="mb-4 font-bold text-3xl text-black dark:text-lime-300 mb-4"><span class="line-through text-gray-500 dark:text-gray-500">$189</span> $149/month</h3>
-                    <div class="inline-block mt-0 mb-4 px-4 py-2 bg-gray-300 dark:bg-gray-900 text-black dark:text-white rounded-md text-xs">Sale Ends 02/31</div>
+                    <h3 class="mb-4 font-bold text-3xl text-black dark:text-lime-300 mb-4">$149/month</h3>
                     <div class="flex items-center justify-center gap-3 mb-6 bg-white dark:bg-gray-700 dark:border-gray-400 bg-opacity-70 dark:bg-opacity-30 rounded-lg p-2 text-gray-700 dark:text-gray-300">
                         <svg class="w-5 h-5 text-green-500 dark:text-green-400 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21.75 6.75a4.5 4.5 0 01-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 11-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 016.336-4.486l-3.776 3.776c.03.242.066.477.098.717l3.464 3.464c.024-.137.048-.274.072-.412l2.341-2.341z"></path>
@@ -141,7 +139,7 @@
                     
                 </div>
                 <div class="relative bg-gray-100 dark:bg-gray-800 pt-8 pb-24 lg:pb-8 px-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                    <h2 class="text-3xl font-bold text-gray-800 dark:text-gray-200">Custom Solutions</h2>
+                    <h2 class="text-4xl font-bold text-gray-800 dark:text-gray-200 mb-2">Custom</h2>
                     <p class="pt-4 leading-[1.5] text-lg text-gray-800 dark:text-lime-300">Sometimes you need something more specific for your business. We also build custom web and mobile apps for our clients.</p>
                     <div class="mt-12 flex justify-center">
                         <svg class="w-full max-w-md h-auto" viewBox="0 0 480 200" fill="none" xmlns="http://www.w3.org/2000/svg">
