14e0f40e8a37d8dfa1044c203e196f4b33eeb4a3
mcozart303 <mikojak@gmail.com>
2026-02-03 07:56:25 -0500

    menu updates

--- resources/views/components/admin-menu.blade.php
+++ resources/views/components/admin-menu.blade.php
@@ -78,22 +78,22 @@
                 </div>
                 <ul class="bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
                     <li>
-                        <a href="{{ route('admin.websites.show', $currentWebsite) }}" 
-                        class="flex items-center p-2 {{ (request()->routeIs('admin.websites.show') || request()->routeIs('admin.websites.edit')) && optional(request()->route('website'))->id === $currentWebsite->id ? 'bg-gray-600 text-white' : 'text-gray-800 dark:text-gray-200 hover:bg-gray-700 hover:text-white' }} transition">
+                        <a href="{{ route('admin.websites.edit', $currentWebsite) }}" 
+                        class="flex items-center p-2 {{ (request()->routeIs('admin.websites.edit')) && optional(request()->route('website'))->id === $currentWebsite->id ? 'bg-gray-600 text-white' : 'text-gray-800 dark:text-gray-200 hover:bg-gray-700 hover:text-white' }} transition">
                             <svg class="w-5 h-5 me-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                             </svg>
-                            Website Configuration
+                            Configuration
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route('admin.websites.images.index', $currentWebsite) }}"
-                        class="flex items-center p-2 {{ request()->routeIs('admin.websites.images.*') && optional(request()->route('website'))->id === $currentWebsite->id ? 'bg-indigo-600 text-white' : 'text-gray-800 dark:text-gray-200 hover:bg-gray-700 hover:text-white' }} transition">
+                        <a href="{{ route('admin.websites.hours.index', $currentWebsite) }}"
+                        class="flex items-center p-2 {{ request()->routeIs('admin.websites.hours.*') && optional(request()->route('website'))->id === $currentWebsite->id ? 'bg-indigo-600 text-white' : 'text-gray-800 dark:text-gray-200 hover:bg-gray-700 hover:text-white' }} transition">
                             <svg class="w-5 h-5 me-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            Website Images
+                            Store Hours
                         </a>
                     </li>
                     <li>
@@ -115,12 +115,12 @@
                         </a>
                     </li>
                     <li>
-                        <a href="{{ route('admin.websites.hours.index', $currentWebsite) }}"
-                        class="flex items-center p-2 {{ request()->routeIs('admin.websites.hours.*') && optional(request()->route('website'))->id === $currentWebsite->id ? 'bg-indigo-600 text-white' : 'text-gray-800 dark:text-gray-200 hover:bg-gray-700 hover:text-white' }} transition">
+                        <a href="{{ route('admin.websites.images.index', $currentWebsite) }}"
+                        class="flex items-center p-2 {{ request()->routeIs('admin.websites.images.*') && optional(request()->route('website'))->id === $currentWebsite->id ? 'bg-indigo-600 text-white' : 'text-gray-800 dark:text-gray-200 hover:bg-gray-700 hover:text-white' }} transition">
                             <svg class="w-5 h-5 me-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                             </svg>
-                            Store Hours
+                            Website Images
                         </a>
                     </li>
                 </ul>
